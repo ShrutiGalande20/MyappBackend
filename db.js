22f1f86242ff6b70a110bacaf194af0aa8051457
Shruti <shrutigalande2@gmail.com>
2024-07-25 20:52:40 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,28 @@
+export const course= [
+    {
+       id:1,
+       courseName: "MARN",
+
+    },
+
+    {
+       id:2,
+       courseName: "MEAN",
+
+    },
+    {
+       id:3,
+       courseName: "middleware",
+
+    },
+    {
+      id:4,
+      courseName: "Full stack",
+
+   }, {
+      id:6,
+      courseName: "back end",
+
+   },
+ ]
+ 
